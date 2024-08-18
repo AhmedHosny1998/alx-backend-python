5558e4185deada88856471d1d3898ac0f6da5759
AhmedHosny1998 <hosny4620@gmail.com>
2024-08-18 15:58:27 -0700

    annotation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,7 +2,8 @@
 '''Task 0's module.
 '''
 
+
 def add(a: float, b: float) -> float:
-	'''Adds two floating-point numbers.
-    	'''
+    '''Adds two floating-point numbers.
+    '''
     return a + b
